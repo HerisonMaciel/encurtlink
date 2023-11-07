6d0e2d8171f7446fa91deedc35059b19f215a560
HerisonMaciel <herisson2maciel@hotmail.com>
2023-11-07 16:54:51 -0300

    first commit

---
+++ src/main/java/com/example/encurtlink/EncurtlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.encurtlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/encurtlink/EncurtlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encurtlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
